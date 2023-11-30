0378881a5f2d8bbaadffed8b46bd13eff4c14454
longph <longphamduy2002@gmail.com>
2023-11-30 22:06:16 +0200

    Add initial layout

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  const commands = ["help", "whoami", "projects", "contact", "clear"] as const;
+  import CommandInput from "./lib/components/CommandInput.svelte";
+  let command = "";
+</script>
+
+<main>
+<p><br/>Welcome to my interactive web terminal.</p>
+<p><br/>For a list of available commands, type 'help'.</p>
+  <CommandInput bind:command={command} />
+</main>
+
+<style>
+  p {
+    position: relative;
+    left: 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    height: 100%;
+    padding: 0 10px;
+    color: #B89076;
+  }
+</style>
---
+++ src/lib/components/CommandInput.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let command = "";
+</script>
+
+<div id="command-input">
+    <label>visitor@longph.com:~$</label><input type="text" bind:value={command}/>
+</div>
+
+<style>
+    label {
+        color: forestgreen;
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    input {
+        font-family: Consolas, Consolas Bold, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+        background: transparent;
+        border: none;
+        color: #73ABAD;
+        font-size: 15px;
+        font-weight: bold;
+        outline: none;
+        width: 600px;
+        margin: 0 10px 0 10px;
+    }
+
+    #command-input {
+        position: relative;
+        left: 10px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        width: 100%;
+        height: 100%;
+        padding: 0 10px;
+    }
+</style>

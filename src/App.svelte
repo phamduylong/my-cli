5727ad653d19511efd965910869b590d376f7771
longph <longphamduy2002@gmail.com>
2024-11-09 19:10:50 +0200

    Update main page with actual command output

--- src/App.svelte
+++ src/App.svelte
@@ -2,40 +2,47 @@
   const commands = ["help", "whoami", "projects", "contact", "clear"] as const;
   import CommandInput from "./lib/components/CommandInput.svelte";
   let command = "";
-  const handleCommand = (e) => {
-    switch(e.detail) {
+  let output = "";
+
+  function handleCommand(e: { detail: string }) {
+    switch (e.detail.trim().toLowerCase()) {
       case "help":
-        console.log("Available commands: ", commands);
+        output = `<b>Available commands:</b><div><ul>${commands.map((command) => `<li>&nbsp;&nbsp;${command}</li>`).join("")}</ul></div>`;
         break;
       case "whoami":
-        console.log("I'm Long Pham, a software engineer based in Espoo, Finland.");
+        output = "I'm Long Pham, a software engineer based in Espoo, Finland.";
         break;
       case "projects":
-        console.log(`You can find my projects <a href="https://github.com/phamduylong">here</a>`);
+        output = `You can find my projects&nbsp;<a class="underline" href="https://longph.com/projects">here</a>.`;
+        break;
+      case "contact":
+        output = `You can contact me via email at <a class="underline" href="mailto:longphamduy2002@gmail.com">longphamduy2002@gmail.com</a> 
+        or <a class="underline" href="https://longph.com/contact" target="_blank"> write to me.</a>`;
+        break;
+      case "clear":
+        output = "";
+        break;
+      case "":
+        break;
       default:
-        console.log("Command not found. Type 'help' for a list of available commands.");
+        output =
+          "Command not found. Type 'help' for a list of available commands.";
         break;
     }
   }
 </script>
 
 <main>
-<p><br/>Welcome to my interactive web terminal.</p>
-<p><br/>For a list of available commands, type 'help'.</p>
-  <CommandInput on:command={handleCommand} bind:command={command} />
+  <div class="flex flex-col container p-4 !text-[#b89076] !overflow-hidden">
+    <div class="font-bold">
+      Welcome to my interactive web terminal.
+    </div>
+    <div class="font-bold">
+      For a list of available commands, type 'help'.
+    </div>
+    <CommandInput on:command={handleCommand} bind:command />
+    {#if output}
+      <div class="space-x-1">&#8618;&nbsp;{@html output}</div>
+    {/if}
+  </div>
 </main>
-
-<style>
-  p {
-    position: relative;
-    left: 10px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    width: 100%;
-    height: 100%;
-    padding: 0 10px;
-    color: #B89076;
-  }
-</style>

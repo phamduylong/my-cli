82d1b7929bff5c76e65ee17ebf124edd13214ede
longph <longphamduy2002@gmail.com>
2024-11-10 01:51:14 +0200

    Implement command history system

--- src/lib/components/CommandInput.svelte
+++ src/lib/components/CommandInput.svelte
@@ -2,31 +2,52 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let command = "";
-  let lastCommand = "";
+  let commandHistory: string[] = JSON.parse(
+    localStorage.getItem("commandHistory") ?? "[]"
+  );
+  let lastCommandIdx = commandHistory.length === 0 ? 0 : commandHistory.length;
 </script>
 
 <div class="space-x-2 flex !flex-row">
   <label for="command" class="!text-[forestgreen] font-bold"
     >visitor@longph.com:~$</label
   ><input
-    class="bg-[#151515] border-none text-[#73abad] font-bold outline-none overflow-scroll"
+    class="bg-[#151515] border-none text-[#73abad] w-full font-bold outline-none overflow-scroll"
     autofocus={true}
     name="command"
     autocomplete="off"
     type="text"
     bind:value={command}
     on:keydown={(e) => {
-      if (e.key === "Enter") {
-        lastCommand = command;
-        dispatch("command", command);
-        command = "";
-      }
-      if (e.key === "ArrowUp") {
-        console.log("ArrowUp, ", lastCommand);
-        e.preventDefault();
-        command = lastCommand;
-        dispatch("command", command);
-        lastCommand = command;
+      switch (e.key) {
+        case "ArrowUp":
+          e.preventDefault();
+          if (lastCommandIdx > 0) {
+            lastCommandIdx -= 1;
+          }
+          command = commandHistory[lastCommandIdx];
+          break;
+        case "ArrowDown":
+          e.preventDefault();
+          if (lastCommandIdx < commandHistory.length - 1) {
+            lastCommandIdx += 1;
+          }
+          command = commandHistory[lastCommandIdx];
+          break;
+        case "Enter":
+          commandHistory = [...commandHistory, command.trim()];
+          localStorage.setItem(
+            "commandHistory",
+            JSON.stringify(commandHistory)
+          );
+          lastCommandIdx = commandHistory.length - 1;
+          dispatch("command", command.trim());
+          command = "";
+          break;
+        default:
+          // FIXME: Any key will trigger this, we only want to do this when text changed. For some reason, on:change doesn't work for text deletion
+          lastCommandIdx = commandHistory.length;
+          break;
       }
     }}
   />
